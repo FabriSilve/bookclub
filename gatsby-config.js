894cb6b5a1409a507fd51d69033e3d1e82350331
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2020-04-03 19:14:16 +0200

    update meda data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `BookClub`,
+    description: `Un posto tranquillo in cui uomini di cultura possono confrontarsi.`,
+    author: `@FabriSilve`,
   },
   pathPrefix: "/bookclub",
   plugins: [

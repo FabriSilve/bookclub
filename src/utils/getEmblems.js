90dbc28021e0641effdd5388f9365e551f12c0ab
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2020-04-11 17:05:00 +0200

    add emblems

--- src/utils/getEmblems.js
+++ src/utils/getEmblems.js
@@ -1,6 +1,36 @@
 import none from '../images/emblems/none.png';
+import ajax from '../images/emblems/ajax.png';
+import arsenal from '../images/emblems/arsenal.png';
+import atleticoMadrid from '../images/emblems/atletico-madrid.png';
+import bayernLeverkusen from '../images/emblems/bayern-leverkusen.png';
+import chelsea from '../images/emblems/chelsea.png';
+import everton from '../images/emblems/everton.png';
+import inter from '../images/emblems/inter.png';
+import lazio from '../images/emblems/lazio.png';
+import manchesterUtd from '../images/emblems/manchester-utd.png';
+import milan from '../images/emblems/milan.png';
+import napoli from '../images/emblems/napoli.png';
+import olympiqueLyonnaise from '../images/emblems/olympique-lyonnaise.png';
+import roma from '../images/emblems/roma.png';
+import tottenham from '../images/emblems/tottenham.png';
+import valencia from '../images/emblems/valencia.png';
 
 const emblems = {
+  'ajax': ajax,
+  'arsenal': arsenal,
+  'atletico-madrid': atleticoMadrid,
+  'bayern-leverkusen': bayernLeverkusen,
+  'chelsea': chelsea,
+  'everton': everton,
+  'inter': inter,
+  'lazio': lazio,
+  'manchester-utd': manchesterUtd,
+  'milan': milan,
+  'napoli': napoli,
+  'olympique-lyonnaise': olympiqueLyonnaise,
+  'roma': roma,
+  'tottenham': tottenham,
+  'valencia': valencia,
 };
 
 const getEmblem = (team) => {

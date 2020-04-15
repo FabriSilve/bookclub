0131e62b783890ef3cf5771b5288ace13bb1bb1e
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2020-04-15 09:04:57 +0200

    Add app manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,13 +28,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `ClubDelLibro`,
+        short_name: `CdL`,
         start_url: `/`,
         background_color: `#171a1c`,
         theme_color: `#171a1c`,
-        display: `minimal-ui`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        display: `standalone`,
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
